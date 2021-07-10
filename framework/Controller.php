3645d0508b349c3524a705c82e784107f0294ff4
Gustavo Henrique Pinto <ghp201@gmail.com>
2021-07-10 10:30:08 -0300

    Provide better comments to index.php and Controller.php

--- framework/Controller.php
+++ framework/Controller.php
@@ -2,8 +2,10 @@
 
 class Controller
 {
-    protected function view(string $view) : int
+    protected function view(string $view)
     {
+        // Get the view name as 'dir.view' and convert it to a valid system
+        // system directory, ex: 'dir/view' on UNIX or 'dir\view' on windows
         $path = str_replace('%.%', DIRECTORY_SEPARATOR, $view);
 
         return require_once "app/Views/{$path}.php";
--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 
+// Get the URL path without using a $_GET['htaccess_key'] approach
 $path = "{$_SERVER['REQUEST_URI']}/";
 
 $separator = explode('/', $path);
 
 // In this specific example the controller will always be the first URL parameter
-$controller = $separator[1] == null ? 'IndexController' : $separator[1];
+$controller = $separator[1] == null ? 'index' : $separator[1];
 $action = $separator[2] == null ? 'index' : $separator[2];
 
-// Requires the controller based on the URL
-require_once "app/Controllers/{$controller}.php";
+// Converts the controller name to PascalCase, so 'index' -> 'Index'
+$controller = mb_convert_case($controller, MB_CASE_TITLE, 'UTF-8');
 
+// Requires the controller based on the URL
+require_once "app/Controllers/{$controller}Controller.php";
+
+// Instance the user desired Controller and call for the action method
 $app = new $controller();
 
 $app->$action();
